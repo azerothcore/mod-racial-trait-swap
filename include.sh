e3a618942a81359aaaea08974f775e06447be55b
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2021-10-14 13:51:42 +0200

    fix(SQL): Build

--- include.sh
+++ include.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 ## GETS THE CURRENT MODULE ROOT DIRECTORY
-MOD_SKELETON_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/" && pwd )"
+MOD_RACIAL_TRAIT_SWAP_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/" && pwd )"
 
-source $MOD_SKELETON_ROOT"/conf/conf.sh.dist"
+source $MOD_RACIAL_TRAIT_SWAP_ROOT"/conf/conf.sh.dist"
 
-if [ -f $MOD_SKELETON_ROOT"/conf/conf.sh" ]; then
-    source $MOD_SKELETON_ROOT"/conf/conf.sh"
+if [ -f $MOD_RACIAL_TRAIT_SWAP_ROOT"/conf/conf.sh" ]; then
+    source $MOD_RACIAL_TRAIT_SWAP_ROOT"/conf/conf.sh"
 fi
